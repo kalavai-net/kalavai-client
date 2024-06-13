6beff17ded3c3213be00db96c13221efeb181828
Carlos Fernandez Musoles <carlos@kalavai.net>
2024-06-13 10:56:45 +0100

    removed docker install

--- scripts/apt_connect.sh
+++ scripts/apt_connect.sh
@@ -70,24 +70,24 @@ install_core_dependencies() {
 
     # docker
     # Add Docker's official GPG key:
-    $SUDO apt-get update
-    $SUDO apt-get install ca-certificates curl
-    $SUDO install -m 0755 -d /etc/apt/keyrings
-    $SUDO curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    $SUDO chmod a+r /etc/apt/keyrings/docker.asc
+    # $SUDO apt-get update
+    # $SUDO apt-get install ca-certificates curl
+    # $SUDO install -m 0755 -d /etc/apt/keyrings
+    # $SUDO curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+    # $SUDO chmod a+r /etc/apt/keyrings/docker.asc
 
-    # Add the repository to Apt sources:
-    echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-    $SUDO tee /etc/apt/sources.list.d/docker.list > /dev/null
-    $SUDO apt-get update
-    $SUDO apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    $SUDO groupadd docker
-    $SUDO usermod -aG docker $USER
-    #newgrp docker
-    $SUDO nvidia-ctk runtime configure --runtime=docker
-    $SUDO systemctl restart docker
+    # # Add the repository to Apt sources:
+    # echo \
+    # "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    # $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    # $SUDO tee /etc/apt/sources.list.d/docker.list > /dev/null
+    # $SUDO apt-get update
+    # $SUDO apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    # $SUDO groupadd docker
+    # $SUDO usermod -aG docker $USER
+    # #newgrp docker
+    # $SUDO nvidia-ctk runtime configure --runtime=docker
+    # $SUDO systemctl restart docker
 
     info "Installing nvidia runtime..."
     curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | $SUDO gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
