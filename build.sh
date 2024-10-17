39ea964593b8ce3b0219b6ef9141f362ad4cf351
musoles <135031143+musoles@users.noreply.github.com>
2024-10-17 21:46:04 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,6 +13,6 @@ echo "## GENERATE INSTALLABLE with FPM"
 # can add dependencies by passing -d name to fpm
 cd build_specs &&
 fpm -s dir -t deb -d wireguard --name kalavai --force &&
-fpm -s dir -t rpm -d wireguard --name kalavai --force && 
+fpm -s dir -t rpm -d wireguard-tools netclient nvidia-container-toolkit --name kalavai --force && 
 mv -i *.deb ../ &&
 mv -i *.rpm ../
