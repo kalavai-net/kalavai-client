b44daee5631f4e140e6b046b43a23b219a342572
Carlos Fernandez Musoles <carlos@kalavai.net>
2024-11-06 16:28:47 +0000

    updated ray log level for templates

--- templates/vllm/ray_init.sh
+++ templates/vllm/ray_init.sh
@@ -49,7 +49,7 @@ case "$subcommand" in
     for (( i=0; i < $ray_init_timeout; i+=5 )); do
       memory=$(echo "$ray_object_store_memory*0.75" | bc -l)
       round_mem=$(round ${memory} 0)
-      ray start --address=$ray_address:$ray_port $ray_block --object-store-memory=$round_mem
+      RAY_BACKEND_LOG_LEVEL=error ray start --address=$ray_address:$ray_port $ray_block --object-store-memory=$round_mem
       if [ $? -eq 0 ]; then
         echo "Worker: Ray runtime started with head address $ray_address:$ray_port"
         exit 0
@@ -95,7 +95,7 @@ case "$subcommand" in
     # start the ray daemon
     memory=$(echo "$ray_object_store_memory*0.75" | bc -l)
     round_mem=$(round ${memory} 0)
-    ray start --head --port=$ray_port --object-store-memory=$round_mem $ray_block
+    RAY_BACKEND_LOG_LEVEL=error ray start --head --port=$ray_port --object-store-memory=$round_mem $ray_block
 
     # wait until all workers are active
     for (( i=0; i < $ray_init_timeout; i+=5 )); do
