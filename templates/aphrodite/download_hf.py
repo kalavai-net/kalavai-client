f56c4b4e0c26c710f6d77f5c47293ac5a4083ae8
Carlos Fernandez Musoles <carlos@kalavai.net>
2024-11-12 13:07:15 +0000

    safely create model cache for aphrodite

--- templates/aphrodite/download_hf.py
+++ templates/aphrodite/download_hf.py
@@ -1,25 +1,24 @@
 import argparse
+from pathlib import Path
 
 from huggingface_hub import (
     hf_hub_download,
     snapshot_download
 )
 
-CACHE_DIR = "/dev/shm"
-
 
 def download_repo(repo_id, local_dir=None):
     return snapshot_download(
         repo_id=repo_id,
         local_dir=local_dir,
-        cache_dir=CACHE_DIR)
+        cache_dir=local_dir)
 
 def download_model_file(repo_id, filename, local_dir=None):
     return hf_hub_download(
         repo_id=repo_id,
         filename=filename,
         local_dir=local_dir,
-        cache_dir=CACHE_DIR)
+        cache_dir=local_dir)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -29,6 +28,8 @@ if __name__ == "__main__":
     parser.add_argument("--local_dir", type=str)
 
     args = parser.parse_args()
+
+    Path(args.local_dir).mkdir(parents=True, exist_ok=True)
 
     if args.filename is not None:
         downloaded_model_path = download_model_file(
