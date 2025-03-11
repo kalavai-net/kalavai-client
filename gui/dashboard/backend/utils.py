45eb055149179298a6be9666724caae3d47ec4f1
Carlos Fernandez Musoles <carlos@kalavai.net>
2025-03-11 21:07:57 +0000

    env vars to control bridge api

--- gui/dashboard/backend/utils.py
+++ gui/dashboard/backend/utils.py
@@ -1,11 +1,14 @@
 import os
 import requests
-from urllib.parse import urljoin
 
 
-KALAVAI_CORE_URL = os.getenv("KALAVAI_CORE_URL", "http://localhost:8001")
+KALAVAI_BRIDGE_URL = os.getenv("KALAVAI_BRIDGE_URL", "http://0.0.0.0")
+KALAVAI_BRIDGE_PORT = os.getenv("KALAVAI_BRIDGE_PORT", "8001")
 
-def request_to_kalavai_core(method, endpoint, base_url=KALAVAI_CORE_URL, params=None, json=None):
+
+def request_to_kalavai_core(method, endpoint, base_url=None, params=None, json=None):
+    if base_url is None:
+        base_url = f"{KALAVAI_BRIDGE_URL}:{KALAVAI_BRIDGE_PORT}"
     result = requests.request(
         method,
         url=f"{base_url}/{endpoint}",
